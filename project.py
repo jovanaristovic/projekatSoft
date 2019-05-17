8ee41b5cd8d1cd8acbb5c5ee39a26f5b97822e4a
jovanaristovic <jovanaristovic@uns.ac.rs>
2019-05-17 18:38:04 +0200

    First commit

---
+++ project.py
@@ -0,0 +1,110 @@
+import numpy as np
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.rcParams['figure.figsize'] = 16, 12
+
+
+
+def load_image(path):
+    return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
+def image_gray(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+def image_bin(image_gs):
+    height, width = image_gs.shape[0:2]
+    image_binary = np.ndarray((height, width), dtype=np.uint8)
+    ret,image_bin = cv2.threshold(image_gs, 127, 255, cv2.THRESH_BINARY)
+    return image_bin
+def invert(image):
+    return 255-image
+def display_image(image, color= False):
+    if color:
+        plt.imshow(image)
+    else:
+        plt.imshow(image, 'gray')
+def dilate(image):
+    kernel = np.ones((3,3)) # strukturni element 3x3 blok
+    return cv2.dilate(image, kernel, iterations=1)
+def erode(image):
+    kernel = np.ones((3,3)) # strukturni element 3x3 blok
+    return cv2.erode(image, kernel, iterations=1)
+
+def resize_region(region):
+     # '''Transformisati selektovani region na sliku dimenzija 28x28'''
+    return cv2.resize(region,(28,28), interpolation = cv2.INTER_NEAREST)
+
+
+
+def findLine(frame):
+    img_org = frame.copy()
+
+    gray = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
+
+
+    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 35, 10)
+  #  cv2.imshow('img', image_bin)
+    contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(img_org, contours, -1, (0, 0, 255), 1)
+  #  cv2.imshow('img', img_org)            #sve konture (nema linije)
+
+    contours_numbers = []  # ovde ce biti samo konture koje pripadaju brojevima
+    for contour in contours:  # za svaku konturu
+        center, size, angle = cv2.minAreaRect(
+            contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
+        width, height = size
+        if height < 50:  # uslov da kontura pripada broju
+            contours_numbers.append(contour)  # ova kontura pripada broju
+
+    img = frame.copy()
+    cv2.drawContours(img, contours_numbers, -1, (0, 0, 255), 1)
+
+    # cv2.imshow('img', img)        #oznaceni su samo brojevi
+
+
+
+    sorted_regions = []  # lista sortiranih regiona po x osi (sa leva na desno)
+    regions_array = []
+    for contour in contours_numbers:
+        x, y, w, h = cv2.boundingRect(contour)  # koordinate i velicina granicnog pravougaonika
+        area = cv2.contourArea(contour)
+        if area > 100 and h < 100 and h > 15 and w > 20:
+            # kopirati [y:y+h+1, x:x+w+1] sa binarne slike i smestiti u novu sliku
+            # označiti region pravougaonikom na originalnoj slici (image_orig) sa rectangle funkcijom
+            region = image_bin[y:y + h + 1, x:x + w + 1]
+            cv2.imshow('img', region)
+            regions_array.append([resize_region(region), (x, y, w, h)])
+            cv2.rectangle(img_org, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    regions_array = sorted(regions_array, key=lambda item: item[1][0])
+    sorted_regions = sorted_regions = [region[0] for region in regions_array]
+
+
+
+####################################################################################################################
+
+
+cap = cv2.VideoCapture('videos/video-9.avi')
+
+while True:
+
+    ret, frame = cap.read()
+
+    if ret == True:
+
+        findLine(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #   cv2.imshow('Video',gray)
+
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
