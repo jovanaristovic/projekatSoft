c988e6c6a298063106f05599f20fc7d128437595
jovanaristovic <jovanaristovic@uns.ac.rs>
2019-06-03 22:38:22 +0200

    line and numbers detected

--- project.py
+++ project.py
@@ -34,6 +34,44 @@ def resize_region(region):
      # '''Transformisati selektovani region na sliku dimenzija 28x28'''
     return cv2.resize(region,(28,28), interpolation = cv2.INTER_NEAREST)
 
+def scale_to_range(image): # skalira elemente slike na opseg od 0 do 1
+    ''' Elementi matrice image su vrednosti 0 ili 255.
+        Potrebno je skalirati sve elemente matrica na opseg od 0 do 1
+    '''
+    return image/255
+
+def matrix_to_vector(image):
+    '''Sliku koja je zapravo matrica 28x28 transformisati u vektor sa 784 elementa'''
+    return image.flatten()
+
+
+def prepare_for_ann(regions):
+    '''Regioni su matrice dimenzija 28x28 čiji su elementi vrednosti 0 ili 255.
+        Potrebno je skalirati elemente regiona na [0,1] i transformisati ga u vektor od 784 elementa '''
+    ready_for_ann = []
+    for region in regions:
+        # skalirati elemente regiona
+        # region sa skaliranim elementima pretvoriti u vektor
+        # vektor dodati u listu spremnih regiona
+        scale = scale_to_range(region)
+        ready_for_ann.append(matrix_to_vector(scale))
+
+    return ready_for_ann
+
+def convert_output(alphabet):
+    '''Konvertovati alfabet u niz pogodan za obučavanje NM,
+        odnosno niz čiji su svi elementi 0 osim elementa čiji je
+        indeks jednak indeksu elementa iz alfabeta za koji formiramo niz.
+        Primer prvi element iz alfabeta [1,0,0,0,0,0,0,0,0,0],
+        za drugi [0,1,0,0,0,0,0,0,0,0] itd..
+    '''
+    nn_outputs = []
+    for index in range(len(alphabet)):
+        output = np.zeros(len(alphabet))
+        output[index] = 1
+        nn_outputs.append(output)
+    return np.array(nn_outputs)
+
 
 
 def findLine(frame):
@@ -41,28 +79,53 @@ def findLine(frame):
 
     gray = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
 
-###################          BROJEVI I REGIONI #########################
+###################          BROJEVI    #########################
 
-    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 35, 10)
-  #  cv2.imshow('img', image_bin)
+    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 10)
+    # cv2.imshow('img', image_bin)
     contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     cv2.drawContours(img_org, contours, -1, (0, 0, 255), 1)
-  #  cv2.imshow('img', img_org)            #sve konture (nema linije)
+    # cv2.imshow('img', img_org)
 
     contours_numbers = []  # ovde ce biti samo konture koje pripadaju brojevima
     for contour in contours:  # za svaku konturu
         center, size, angle = cv2.minAreaRect(
             contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
         width, height = size
-        if height < 60:  # uslov da kontura pripada broju
+        if height < 80 and height > 8 and width > 8:  # uslov da kontura pripada broju
             contours_numbers.append(contour)  # ova kontura pripada broju
 
     img = frame.copy()
     cv2.drawContours(img, contours_numbers, -1, (0, 0, 255), 1)
 
-    cv2.imshow('img', img)        #oznaceni su samo brojevi
+    # cv2.imshow('img', img)  # samo brojevi
 
+    ########################## linija #########################################################
+
+    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 5)
+    # cv2.imshow('img', image_bin)
+    contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(img_org, contours, -1, (0, 0, 255), 1)
+    # cv2.imshow('img', img_org)
+
+
+    contour_line = []  # ovde ce biti samo kontura koja pripada liniji
+    for contour in contours:  # za svaku konturu
+        center, size, angle = cv2.minAreaRect(
+            contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
+        width, height = size
+        if  height < 4 and height > 0 and width > 2 and width < 10  :  # uslov da kontura pripada liniji
+            contour_line.append(contour)  # ova kontura pripada broju
+
+    img = frame.copy()
+    cv2.drawContours(img, contour_line, -1, (0, 0, 255), 1)
+
+    cv2.imshow('img', img)  # oznacena je samo linija
+
+
+################################################# izdvajanje brojeva ###########################################
 
 
     sorted_regions = []  # lista sortiranih regiona po x osi (sa leva na desno)
@@ -74,25 +137,16 @@ def findLine(frame):
             # kopirati [y:y+h+1, x:x+w+1] sa binarne slike i smestiti u novu sliku
             # označiti region pravougaonikom na originalnoj slici (image_orig) sa rectangle funkcijom
             region = image_bin[y:y + h + 1, x:x + w + 1]
-      #      cv2.imshow('img', region)
+            # cv2.imshow('img', region)
             regions_array.append([resize_region(region), (x, y, w, h)])
             cv2.rectangle(img_org, (x, y), (x + w, y + h), (0, 255, 0), 2)
     regions_array = sorted(regions_array, key=lambda item: item[1][0])
     sorted_regions = sorted_regions = [region[0] for region in regions_array]
 
-
-
 ####################################################################################################################
 
 
-
-
-
-
-
-
-
-cap = cv2.VideoCapture('videos/video-9.avi')
+cap = cv2.VideoCapture('videos/video-7.avi')
 
 while True:
 
