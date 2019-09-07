11976e57dba0771a3a32cc2d5b49077b8631ac3f
jovanaristovic <jovanaristovic@uns.ac.rs>
2019-09-07 17:12:13 +0200

    radi nesto

--- project.py
+++ project.py
@@ -1,11 +1,21 @@
 import numpy as np
+import os
+import sys
+from keras.models import model_from_json
 import cv2
 import matplotlib
+import tensorflow as tf
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from keras.datasets import mnist
+from basicFunctions import distance2D, pnt2line
 
-matplotlib.rcParams['figure.figsize'] = 16, 12
 
 
+matplotlib.rcParams['figure.figsize'] = 16, 1
 
 def load_image(path):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
@@ -20,7 +30,7 @@ def invert(image):
     return 255-image
 def display_image(image, color= False):
     if color:
-        plt.imshow(image)
+        cv2.imshow(image)
     else:
         plt.imshow(image, 'gray')
 def dilate(image):
@@ -30,23 +40,26 @@ def erode(image):
     kernel = np.ones((3,3)) # strukturni element 3x3 blok
     return cv2.erode(image, kernel, iterations=1)
 
+
 def resize_region(region):
-     # '''Transformisati selektovani region na sliku dimenzija 28x28'''
+    '''Transformisati selektovani region na sliku dimenzija 28x28'''
+
+
     return cv2.resize(region,(28,28), interpolation = cv2.INTER_NEAREST)
+
 
 def scale_to_range(image): # skalira elemente slike na opseg od 0 do 1
     ''' Elementi matrice image su vrednosti 0 ili 255.
         Potrebno je skalirati sve elemente matrica na opseg od 0 do 1
     '''
     return image/255
-
 def matrix_to_vector(image):
     '''Sliku koja je zapravo matrica 28x28 transformisati u vektor sa 784 elementa'''
     return image.flatten()
 
 
 def prepare_for_ann(regions):
-    '''Regioni su matrice dimenzija 28x28 čiji su elementi vrednosti 0 ili 255.
+    '''Regioni su matrice dimenzija 28x28 ciji su elementi vrednosti 0 ili 255.
         Potrebno je skalirati elemente regiona na [0,1] i transformisati ga u vektor od 784 elementa '''
     ready_for_ann = []
     for region in regions:
@@ -54,13 +67,14 @@ def prepare_for_ann(regions):
         # region sa skaliranim elementima pretvoriti u vektor
         # vektor dodati u listu spremnih regiona
         scale = scale_to_range(region)
+        # scale = region.reshape(1,28,28,1)
         ready_for_ann.append(matrix_to_vector(scale))
 
     return ready_for_ann
 
 def convert_output(alphabet):
-    '''Konvertovati alfabet u niz pogodan za obučavanje NM,
-        odnosno niz čiji su svi elementi 0 osim elementa čiji je
+    '''Konvertovati alfabet u niz pogodan za obucavanje NM,
+        odnosno niz ciji su svi elementi 0 osim elementa ciji je
         indeks jednak indeksu elementa iz alfabeta za koji formiramo niz.
         Primer prvi element iz alfabeta [1,0,0,0,0,0,0,0,0,0],
         za drugi [0,1,0,0,0,0,0,0,0,0] itd..
@@ -73,25 +87,82 @@ def convert_output(alphabet):
     return np.array(nn_outputs)
 
 
+def create_ann():
+    '''Implementacija veštacke neuronske mreže sa 784 neurona na uloznom sloju,
+        128 neurona u skrivenom sloju i 10 neurona na izlazu. Aktivaciona funkcija je sigmoid.
+    '''
+    ann = Sequential()
+    ann.add(Dense(128, input_dim=784, activation='sigmoid'))
+    ann.add(Dense(10, activation='sigmoid'))
+    return ann
 
-def findLine(frame):
+
+def load_rgb_image(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+def in_rangeImage(image, lower, upper):
+    image = load_rgb_image(image)
+    mask = cv2.inRange(image, lower, upper)
+    return cv2.bitwise_and(image, image, mask=mask)
+
+
+def train_ann(ann, X_train, y_train):
+    '''Obucavanje vestacke neuronske mreze'''
+    X_train = np.array(X_train, np.float32)  # dati ulazi
+    y_train = np.array(y_train, np.float32)  # zeljeni izlazi za date ulaze
+
+    # definisanje parametra algoritma za obucavanje
+    sgd = SGD(lr=0.01, momentum=0.9)
+    ann.compile(loss='mean_squared_error', optimizer=sgd)
+
+    # obucavanje neuronske mreze
+    ann.fit(X_train, y_train, epochs=2000, batch_size=1, verbose=0, shuffle=False)
+
+    return ann
+
+def winner(output): # output je vektor sa izlaza neuronske mreze
+    '''pronaci i vratiti indeks neurona koji je najviše pobuden'''
+    return max(enumerate(output), key=lambda x: x[1])[0]
+
+# def display_result(outputs, alphabet):
+#     '''za svaki rezultat pronaci indeks pobednickog
+#         regiona koji ujedno predstavlja i indeks u alfabetu.
+#         Dodati karakter iz alfabet u result'''
+#     result = []
+#     for output in outputs:
+#         result.append(alphabet[winner(output)])
+#     return result
+
+def display_result(outputs): # dodaje u niz broj koji je prepoznat
+    '''za svaki rezultat pronaci indeks pobednickog
+        regiona koji ujedno predstavlja i indeks u alfabetu.
+        Dodati karakter iz alfabet u result'''
+
+    alphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    result = []
+    for output in outputs:
+        result.append(alphabet[winner(output)])
+    return result
+
+
+###########################################  BROJEVI    ###################################################
+
+
+def detectNumbers(frame):
     img_org = frame.copy()
 
-    gray = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
-
-###################          BROJEVI    #########################
-
-    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 10)
+    gray = image_gray(img_org)
+    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 25)
     # cv2.imshow('img', image_bin)
-    contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    _ ,contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     cv2.drawContours(img_org, contours, -1, (0, 0, 255), 1)
     # cv2.imshow('img', img_org)
 
     contours_numbers = []  # ovde ce biti samo konture koje pripadaju brojevima
     for contour in contours:  # za svaku konturu
-        center, size, angle = cv2.minAreaRect(
-            contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
+        center, size, angle = cv2.minAreaRect(contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
         width, height = size
         if height < 80 and height > 8 and width > 8:  # uslov da kontura pripada broju
             contours_numbers.append(contour)  # ova kontura pripada broju
@@ -101,65 +172,249 @@ def findLine(frame):
 
     # cv2.imshow('img', img)  # samo brojevi
 
-    ########################## linija #########################################################
-
-    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 5)
-    # cv2.imshow('img', image_bin)
-    contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-    cv2.drawContours(img_org, contours, -1, (0, 0, 255), 1)
-    # cv2.imshow('img', img_org)
+    return contours_numbers
 
 
-    contour_line = []  # ovde ce biti samo kontura koja pripada liniji
-    for contour in contours:  # za svaku konturu
-        center, size, angle = cv2.minAreaRect(
-            contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
-        width, height = size
-        if  height < 4 and height > 0 and width > 2 and width < 10  :  # uslov da kontura pripada liniji
-            contour_line.append(contour)  # ova kontura pripada broju
+def select_roi(image_orig,contours_numbers, linePlus, lineMinus):
 
-    img = frame.copy()
-    cv2.drawContours(img, contour_line, -1, (0, 0, 255), 1)
-
-    cv2.imshow('img', img)  # oznacena je samo linija
-
-
-################################################# izdvajanje brojeva ###########################################
-
+    img_org = frame.copy()
+    gray = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
+    image_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 10)
 
     sorted_regions = []  # lista sortiranih regiona po x osi (sa leva na desno)
-    regions_array = []
+    regions_arrayPlava = []
+    regions_arrayZelena = []
+
+    tacke_arrayPlava = []
+    tacke_arrayZelena = []
+
+    [(xPlavaD, yPlavaG), (xPlavaG, yPlavaD)] = linePlus
+
+    [(xZelenaD, yZelenaG), (xZelenaG, yZelenaD)] = lineMinus
+
+    # linije
     for contour in contours_numbers:
         x, y, w, h = cv2.boundingRect(contour)  # koordinate i velicina granicnog pravougaonika
+
+        distancePlava, _, preciCePlavu = pnt2line((x, y), (xPlavaD, yPlavaG), (xPlavaG, yPlavaD))
+
+        img = frame.copy()
+        cv2.drawContours(img, contour, -1, (0, 0, 255), 1)
+
+        cv2.imshow('img', img)  # samo brojevi
+        # print('Distanca od plave linije je: ', distancePlava)
+
+        distanceZelena, _, preciCeZelenu = pnt2line((x, y), (xZelenaD, yZelenaG), (xZelenaG, yZelenaD))
+        # print('Distanca od zelene linije je: ', distanceZelena)
+
         area = cv2.contourArea(contour)
-        if area > 100 and h < 100 and h > 15 and w > 20:
+
+        if ((h >= 10 and w >= 6) and (h <= 28 and w <= 28)):
             # kopirati [y:y+h+1, x:x+w+1] sa binarne slike i smestiti u novu sliku
             # označiti region pravougaonikom na originalnoj slici (image_orig) sa rectangle funkcijom
-            region = image_bin[y:y + h + 1, x:x + w + 1]
-            # cv2.imshow('img', region)
-            regions_array.append([resize_region(region), (x, y, w, h)])
-            cv2.rectangle(img_org, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    regions_array = sorted(regions_array, key=lambda item: item[1][0])
-    sorted_regions = sorted_regions = [region[0] for region in regions_array]
 
-####################################################################################################################
+            # sabiranje - plava linija
+            if (preciCePlavu == True and distancePlava <= 15):
+                tackaPlava = (x, y)
+                tacke_arrayPlava.append(tackaPlava)
+
+                regionPlava = image_bin[y:y + h + 1, x:x + w + 1]
+                regions_arrayPlava.append([resize_region(regionPlava), (x, y, w, h)])
+                cv2.rectangle(image_orig, (x, y), (x + w, y + h), (100, 80, 110), 2)
+
+            # oduzimanje - zelena linija
+            if (preciCeZelenu == True and distanceZelena <= 15):
+                tackaZelena = (x, y)
+                tacke_arrayZelena.append(tackaZelena)
+
+                regionZelena = image_bin[y:y + h + 1, x:x + w + 1]
+                regions_arrayZelena.append([resize_region(regionZelena), (x, y, w, h)])
+                cv2.rectangle(image_orig, (x, y), (x + w, y + h), (100, 80, 110), 2)
+
+    # sortirani regioni za plavu liniju
+    regions_arrayPlava = sorted(regions_arrayPlava, key=lambda item: item[1][0])
+    sorted_regionsPlava = sorted_regionsPlava = [region[0] for region in regions_arrayPlava]
+
+    # sortirani regioni za zelenu liniju
+    regions_arrayZelena = sorted(regions_arrayZelena, key=lambda item: item[1][0])
+    sorted_regionsZelena = sorted_regionsZelena = [region[0] for region in regions_arrayZelena]
+
+    # sortirati sve regione po x osi (sa leva na desno) i smestiti u promenljivu sorted_regions
+    return image_orig, sorted_regionsPlava, sorted_regionsZelena, x, y, tacke_arrayPlava, tacke_arrayZelena
+
+##############################################    LINIJA      #########################################################
+
+def detectLineHog(img):
+
+    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # konverzija BGR u RGB
+    gray = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)  # slika konvertovana u crno-belu
+    ret, t = cv2.threshold(gray, 25, 255, cv2.THRESH_BINARY)  # primena binarnog tresholda
+    edges = cv2.Canny(img, 50, 150, apertureSize=3)
+
+    # primena Hjuove transformacije sa prosledjenim parametrima
+    lines = cv2.HoughLinesP(image=edges, rho=1, theta=np.pi / 180 , threshold=100,
+                            minLineLength=100, maxLineGap=100)
+
+    xDonje, yDonje, xGornje, yGornje = [], [], [], []  # donja leva, donja desna, gornja leva, gornja desna
+    # ima 4 jer je linija deblja
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                xDonje.append(x1)
+                yDonje.append(y2)
+                xGornje.append(x2)
+                yGornje.append(y1)
+        xDonje1, yDonje1, xGornje1, yGornje1 = min(xDonje), min(yDonje), max(xGornje), max(yGornje)
+
+        return xDonje1, yGornje1, xGornje1, yDonje1  # vraca koordinate
+
+    # ovu pozivam za pronalazak plave
 
 
-cap = cv2.VideoCapture('videos/video-7.avi')
+def findBlueLine2(frame):
+    lower = np.array([0, 0, 100], dtype="uint8")
+    upper = np.array([50, 50, 255], dtype="uint8")
+    blue = in_rangeImage(frame, lower, upper)
+    xmin1, ymax1, xmax1, ymin1 = detectLineHog(blue)
+    blueLine = (xmin1, ymax1), (xmax1, ymin1)
 
+    # cv2.imshow('blueLine', blue)
+    return blueLine
+
+def findGreenLine2(frame):
+    lower = np.array([0, 180, 0 ], dtype="uint8")
+    upper = np.array([50, 255, 50], dtype="uint8")
+    green = in_rangeImage(frame, lower, upper)
+    xmin2, ymax2, xmax2, ymin2 = detectLineHog(green)
+    greenLine = (xmin2, ymax2), (xmax2, ymin2)
+
+    # cv2.imshow('greenLine', green)
+    return greenLine
+
+
+##########################################################################################################################
+def neuronska():
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train_s = []
+    for x in x_train[:10:]:
+        ret, slika = cv2.threshold(x, 180, 255, cv2.THRESH_BINARY)
+        x_train_s.append(slika)
+
+    ann = create_ann()
+    ann = train_ann(ann, np.array(prepare_for_ann(x_train_s), np.float32), convert_output(y_train[:10:]))
+    model_json = ann.to_json()
+
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+
+    ann.save_weights("model.h5")
+    print("Saved model to disk")
+
+    return ann
+################################################
+
+if os.path.isfile("model.h5"):
+
+    print("File exists!")
+    fileExists=True
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model=model_from_json(loaded_model_json)
+    model.load_weights("model.h5")
+    print("Loaded model from disk")
+else:
+    print("File does not exist!")
+    fileExists=False
+    model=neuronska()
+
+
+
+cap = cv2.VideoCapture('videos/video-9.avi')
+vec_sabrani = []
+vec_oduzeti = []
+
+vec_uzete_tackeSABIRAJ = []
+vec_uzete_tackeODUZMI = []
+
+suma = 0
+sumaSabranih = 0
+sumaOduzetih = 0
 while True:
 
     ret, frame = cap.read()
 
     if ret == True:
 
-        findLine(frame)
-        #
+        # detectedLines = {}
+        plusLine = findBlueLine2(frame)
+        # addLine = detectedLines['plavaLinija']
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        minusLine = findGreenLine2(frame)
+        # subLine = detectedLines['zelenaLinija']
 
-        #   cv2.imshow('Video',gray)
+        contours_numbers = detectNumbers(frame)
+
+        image_orig, sorted_regionsPlava, sorted_regionsZelena, selectRoiX, selectRoiY, tacke_arrayPlava, tacke_arrayZelena = select_roi(frame, contours_numbers, plusLine, minusLine)
+
+        if (len(sorted_regionsPlava) > 0):
+
+            result = model.predict(np.array(prepare_for_ann(sorted_regionsPlava), np.float32))
+
+            rezultat = []
+            rezultat = display_result(result)
+
+
+
+            for r in rezultat:
+                for tackaPlava in tacke_arrayPlava:
+
+                    vec_uzete_tackeSABIRAJ.append(tackaPlava)
+                    vec_sabrani.append(r)
+
+                    if (len(vec_sabrani) >= 2 and len(vec_uzete_tackeSABIRAJ) >= 2):
+                        if (vec_sabrani[len(vec_sabrani) - 2] == vec_sabrani[len(vec_sabrani) - 1]):
+                            if (distance2D(vec_uzete_tackeSABIRAJ[len(vec_uzete_tackeSABIRAJ) - 2],
+                                           vec_uzete_tackeSABIRAJ[len(vec_uzete_tackeSABIRAJ) - 1]) <= 3.5):
+                                print('+')
+                        else:
+                            sumaSabranih += r
+                            print('SABIRAM BROJ: ', r)
+                    else:
+                        print('SABIRAM BROJ: ', r)
+                        sumaSabranih += r
+
+        if (len(sorted_regionsZelena) > 0):
+
+            result = model.predict(np.array(prepare_for_ann(sorted_regionsZelena), np.float32))
+            rezultat = []
+            rezultat = display_result(result)
+
+            for r in rezultat:
+                for tackaZelena in tacke_arrayZelena:
+
+                    vec_uzete_tackeODUZMI.append(tackaZelena)
+                    vec_oduzeti.append(r)
+
+                    if (len(vec_oduzeti) >= 2 and len(vec_uzete_tackeODUZMI) >= 2):
+                        if (vec_oduzeti[len(vec_oduzeti) - 2] == vec_oduzeti[len(vec_oduzeti) - 1]):
+                            if (distance2D(vec_uzete_tackeODUZMI[len(vec_uzete_tackeODUZMI) - 2],
+                                           vec_uzete_tackeODUZMI[len(vec_uzete_tackeODUZMI) - 1]) <= 3.5):
+                                print('-')
+                        else:
+                            sumaOduzetih += r
+                            print('ODUZIMAM BROJ: ', r)
+                    else:
+                        print('ODUZIMAM BROJ: ', r)
+                        sumaOduzetih += r
+
+        suma = sumaSabranih - sumaOduzetih
+        print(suma)
+        cv2.imshow('dc', frame)
+
+
 
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
